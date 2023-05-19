1da4e476628e00d15853ba7cfeb55ce972a5f999
jayakarthik-jk <karthick72002@gmail.com>
2023-05-19 11:58:40 +0530

    frontend url added to cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://certificate-wallet.vercel.app"],
   })
 );
 
